99118953658656ea934df6f748eb3a1c55dd0689
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-07 10:56:06 -0400

    basics of streamlit

--- streamlit/ddsimca.py
+++ streamlit/ddsimca.py
@@ -2,6 +2,23 @@
 Interactive demonstration of DD-SIMCA.
 Author: Nathan A. Mahynski
 """
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+from streamlit_pdf_viewer import pdf_viewer
+from streamlit_tags import st_tags
+
+st.set_page_config(layout="wide")
+
+with st.sidebar:
+    st.title('DD-SIMCA')
+    st.markdown('''
+    ## About this application
+    This tool uses the [PyChemAuth](https://pychemauth.readthedocs.io/en/latest/index.html) python package for analysis.
+    ''')
+    add_vertical_space(2)
+    st.write('Made by ***Nate Mahynski***')
+    st.write('nathan.mahynski@nist.gov')
+
 
 if __name__ == "__main__":
   print('ddsimca')

6e724381227d796fa6619194b759a778c515ae11
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-07 12:36:00 -0400

    debug

--- streamlit/ddsimca.py
+++ streamlit/ddsimca.py
@@ -17,7 +17,7 @@ with st.sidebar:
     This tool uses the [PyChemAuth](https://pychemauth.readthedocs.io/en/latest/index.html) python 
     package for analysis.
     
-    :heavy_check_mark: It is intended to demonstrate the use of DD-SIMCA for modeling data.
+    :heavy_check_mark: It is intended as a teaching tool to demonstrate the use of DD-SIMCA for modeling data.
 
     :x: It is not intended to be used in production.  Instead, use the Jupyter notebooks provided in 
     [PyChemAuth](https://pychemauth.readthedocs.io/en/latest/index.html) for reproducible, high-quality
@@ -42,7 +42,7 @@ if uploaded_file is not None:
 
     with col1:
       target_column = st.selectbox(label="Select a column as the target class.", options=dataframe.columns, index=None, placeholder="Select a column", disabled=False, label_visibility="visible")
-      if target_colums is not None:
+      if target_column is not None:
         target_class = st.selectbox(label="Select a class to model.", options=dataframe[target_column].unique(), index=None, placeholder="Select a class", disabled=False, label_visibility="visible")
       random_state = st.number_input(label="Random seed for data shuffling before stratified splitting.", min_value=None, max_value=None, value=42, step=1, placeholder="Seed", disabled=False, label_visibility="visible")
       test_size = st.slider(label="Select a positive fraction of the data to use as a test set to proceed.", min_value=0.0, max_value=1.0, value=0.0, step=0.05, disabled=False, label_visibility="visible")

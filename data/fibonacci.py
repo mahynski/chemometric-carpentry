f01adf030b6b7263eddd62b425d198ecd4bc7b4d
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-24 12:08:20 -0400

    Create fibonacci.py

---
+++ data/fibonacci.py
@@ -0,0 +1,32 @@
+def Fibonacci(n):
+	"""
+	This is where your Docstring goes.
+
+	Input
+	-----
+	n : int
+		Fibonacci number to get.
+
+	Returns
+	-------
+	number : int
+		The nth Fibonacci number.
+	"""
+
+	# Check if input is 0 then it will
+	# print incorrect input
+	if n < 0:
+		print("Incorrect input")
+
+	# Check if n is 0
+	# then it will return 0
+	elif n == 0:
+		return 0
+
+	# Check if n is 1,2
+	# it will return 1
+	elif n == 1 or n == 2:
+		return 1
+
+	else:
+		return Fibonacci(n-1) + Fibonacci(n-2)

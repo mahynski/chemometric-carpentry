2e0781cb21fe17736998372dc105aec4360c3760
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-29 15:03:27 -0400

    ols intro

--- streamlit/ols.py
+++ streamlit/ols.py
@@ -95,7 +95,7 @@ with col2_:
         background_color=bg_color,
         background_image=None,
         update_streamlit=False,
-        height=700,
+        height=600,
         drawing_mode=drawing_mode,
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         display_toolbar=True,

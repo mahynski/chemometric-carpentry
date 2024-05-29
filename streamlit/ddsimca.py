ed21be4367e9a8953b12e3561485e2ec721d9b5b
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-29 13:00:31 -0400

    test

--- streamlit/ddsimca.py
+++ streamlit/ddsimca.py
@@ -252,7 +252,7 @@ if (uploaded_file is not None) and test_size > 0 and target_column is not None:
       st.write(f"Number of loops = {dds.model._DDSIMCA_Model__sft_history_['outer_loops']}")
       st.dataframe(
         pd.DataFrame(data=dds.model._DDSIMCA_Model__sft_history_['removed']['X'], columns=[c for c in dataframe.columns if c != target_column]),
-        hide_index=False
+        hide_index=True
       )
 
       mask_out = dds.model.check_outliers(X_train)[1]

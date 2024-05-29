8460464577eca4985a6ca418adf10aa5fd057bf3
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-29 09:28:54 -0400

    test

--- streamlit/ddsimca.py
+++ streamlit/ddsimca.py
@@ -95,7 +95,7 @@ with col2_:
         background_color=bg_color,
         background_image=None,
         update_streamlit=True,
-        height=400,
+        height=350,
         drawing_mode=drawing_mode,
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="full app",

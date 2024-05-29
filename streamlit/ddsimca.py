38a4f6a69a5b41060558cf5b8047fd0b0e76f772
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-29 09:18:46 -0400

    test

--- streamlit/ddsimca.py
+++ streamlit/ddsimca.py
@@ -95,6 +95,8 @@ with col2_:
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="canvas",
     )
+    if canvas_result.image_data is not None:
+      st.image(canvas_result.image_data)
 
 
 st.divider() 

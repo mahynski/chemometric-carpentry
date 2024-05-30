f4810663a672eae24113e5aa3817aa11b6cf9740
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-29 20:50:35 -0400

    updated ols data link

--- streamlit/ols.py
+++ streamlit/ols.py
@@ -109,7 +109,7 @@ st.divider()
 st.header("Upload Your Data")
 
 uploaded_file = st.file_uploader(
-  label="Upload a CSV file. Observations should be in rows, while columns should correspond to different features. An example file is available [here](https://github.com/mahynski/chemometric-carpentry/blob/c9a91d65f8f5d151dad40a6aed8044c9654cf48c/data/simca-iris.csv).",
+  label="Upload a CSV file. Observations should be in rows, while columns should correspond to different features. An example file is available [here](https://github.com/mahynski/chemometric-carpentry/blob/7401f28144ebf60a7d4a9455d166eddabbf5e78f/data/ols-hitters.csv).",
   type=['csv'], accept_multiple_files=False, 
   key=None, help="", 
   on_change=None, label_visibility="visible")

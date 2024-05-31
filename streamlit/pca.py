90325a55a9959bf3070d2aadc80e8016ed713953
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-31 11:50:16 -0400

    pca test

--- streamlit/pca.py
+++ streamlit/pca.py
@@ -79,7 +79,7 @@ st.divider()
 st.header("Upload Your Data")
 
 uploaded_file = st.file_uploader(
-  label="Upload a CSV file. Observations should be in rows, while columns should correspond to different features. An example file is available [here]().",
+  label="Upload a CSV file. Observations should be in rows, while columns should correspond to different features. An example file is available [here](https://github.com/mahynski/chemometric-carpentry/blob/c9a91d65f8f5d151dad40a6aed8044c9654cf48c/data/simca-iris.csv).",
   type=['csv'], accept_multiple_files=False, 
   key=None, help="", 
   on_change=None, label_visibility="visible")
@@ -99,11 +99,11 @@ with st.expander("Settings"):
       with col1:
         st.subheader("Data Settings")
 
-        target_column = st.selectbox(label="Select a column as the target.", options=dataframe.columns, index=None, placeholder="Select a column", disabled=False, label_visibility="visible")
+        target_column = st.selectbox(label="Select the column which indicates class, if present.", options=dataframe.columns, index=None, placeholder="Select a column", disabled=False, label_visibility="visible")
         feature_names = [c for c in dataframe.columns if c != target_column]
 
-        random_state = st.number_input(label="Random seed for data shuffling before stratified splitting.", min_value=None, max_value=None, value=42, step=1, placeholder="Seed", disabled=False, label_visibility="visible")
-        test_size = st.slider(label="Select a positive fraction of the data to use as a test set to begin analysis.", min_value=0.0, max_value=1.0, value=0.0, step=0.05, disabled=False, label_visibility="visible")
+        # random_state = st.number_input(label="Random seed for data shuffling before stratified splitting.", min_value=None, max_value=None, value=42, step=1, placeholder="Seed", disabled=False, label_visibility="visible")
+        # test_size = st.slider(label="Select a positive fraction of the data to use as a test set to begin analysis.", min_value=0.0, max_value=1.0, value=0.0, step=0.05, disabled=False, label_visibility="visible")
 
         standardization = st.selectbox("What type of standardization should be applied?", (None, "Scaler", "Robust Scaler"), index=0)
         if standardization is not None:
@@ -113,123 +113,118 @@ with st.expander("Settings"):
       with col2:
         st.subheader("Model Settings")
 
-        # select regularization type
-        reg_type = st.selectbox("What type of regularization should be applied?", (None, "LASSO (L1)", "Ridge (L2)"), index=0)
-
-        # select strength
-        if reg_type is not None:
-          reg_strength = st.select_slider("Regularization strength", options=np.logspace(-6, 6, 25))
+        n_components = st.slider("Number of components to use?", 1, len(feature_names)-1, value=1)
 
 
-if (test_size > 0):
-  X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split(
-    dataframe[feature_names].values,
-    dataframe[target_column].values,
-    dataframe.index.values,
-    shuffle=True,
-    random_state=random_state,
-    test_size=test_size,
-  )
+# if (test_size > 0):
+#   X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split(
+#     dataframe[feature_names].values,
+#     dataframe[target_column].values,
+#     dataframe.index.values,
+#     shuffle=True,
+#     random_state=random_state,
+#     test_size=test_size,
+#   )
 
-  if standardization == "Scaler":
-    scaler = CorrectedScaler(with_mean=center, with_std=scale)
-  elif standardization == "RobustScaler":
-    scaler = RobustScaler(with_median=center, with_iqr=scale)
-  else:
-    scaler = CorrectedScaler(with_mean=False, with_std=False)
+#   if standardization == "Scaler":
+#     scaler = CorrectedScaler(with_mean=center, with_std=scale)
+#   elif standardization == "RobustScaler":
+#     scaler = RobustScaler(with_median=center, with_iqr=scale)
+#   else:
+#     scaler = CorrectedScaler(with_mean=False, with_std=False)
 
-  X_train = scaler.fit_transform(X_train)
-  X_test = scaler.transform(X_test)
+#   X_train = scaler.fit_transform(X_train)
+#   X_test = scaler.transform(X_test)
 
-  data_tab, train_tab, test_tab, results_tab, coef_tab = st.tabs(["Original Data", "Training Data", "Testing Data", "Modeling Results", "Coefficients"])
+#   data_tab, train_tab, test_tab, results_tab, coef_tab = st.tabs(["Original Data", "Training Data", "Testing Data", "Modeling Results", "Coefficients"])
 
-  with data_tab:
-    st.header("Original Data")
-    st.dataframe(dataframe)
+#   with data_tab:
+#     st.header("Original Data")
+#     st.dataframe(dataframe)
 
-  with train_tab:
-    st.header("Training Data")
-    st.dataframe(pd.DataFrame(data=np.hstack((X_train, y_train.reshape(-1,1))), columns=feature_names+[target_column], index=idx_train))
+#   with train_tab:
+#     st.header("Training Data")
+#     st.dataframe(pd.DataFrame(data=np.hstack((X_train, y_train.reshape(-1,1))), columns=feature_names+[target_column], index=idx_train))
 
-  with test_tab:
-    st.header("Testing Data")
-    st.dataframe(pd.DataFrame(data=np.hstack((X_test, y_test.reshape(-1,1))), columns=feature_names+[target_column], index=idx_test))
+#   with test_tab:
+#     st.header("Testing Data")
+#     st.dataframe(pd.DataFrame(data=np.hstack((X_test, y_test.reshape(-1,1))), columns=feature_names+[target_column], index=idx_test))
       
-  with results_tab:
-    st.header("Modeling Results")
+#   with results_tab:
+#     st.header("Modeling Results")
 
-    if reg_type is None:
-      model = linear_model.LinearRegression(fit_intercept=True)
-    elif reg_type == "LASSO (L1)":
-      model = linear_model.Lasso(alpha=reg_strength, fit_intercept=True)
-    else:
-      model = linear_model.Ridge(alpha=reg_strength, fit_intercept=True)
+#     if reg_type is None:
+#       model = linear_model.LinearRegression(fit_intercept=True)
+#     elif reg_type == "LASSO (L1)":
+#       model = linear_model.Lasso(alpha=reg_strength, fit_intercept=True)
+#     else:
+#       model = linear_model.Ridge(alpha=reg_strength, fit_intercept=True)
 
-    _ = model.fit(X_train, y_train)
+#     _ = model.fit(X_train, y_train)
 
-    def configure_plot(ax, size=(2,2)):
-      for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
-        item.set_fontsize(6)
-      fig = plt.gcf()
-      fig.set_size_inches(*size)
-      st.pyplot(fig, use_container_width=False)
+#     def configure_plot(ax, size=(2,2)):
+#       for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
+#         item.set_fontsize(6)
+#       fig = plt.gcf()
+#       fig.set_size_inches(*size)
+#       st.pyplot(fig, use_container_width=False)
 
-    def fit_gaussian(data, ax):
-      from scipy.stats import norm
-      mu, std = norm.fit(data)
-      xmin, xmax = ax.get_xlim()
-      x = np.linspace(xmin, xmax, 100)
-      p = norm.pdf(x, mu, std)
-      ax.plot(x, p, 'k', linewidth=1, label='Guassian Fit')
-      ax.axvline(mu, color='r', label=f'Gaussian Center ({"%.3f"%mu})')
-      ax.legend(loc='best', fontsize=6)
+#     def fit_gaussian(data, ax):
+#       from scipy.stats import norm
+#       mu, std = norm.fit(data)
+#       xmin, xmax = ax.get_xlim()
+#       x = np.linspace(xmin, xmax, 100)
+#       p = norm.pdf(x, mu, std)
+#       ax.plot(x, p, 'k', linewidth=1, label='Guassian Fit')
+#       ax.axvline(mu, color='r', label=f'Gaussian Center ({"%.3f"%mu})')
+#       ax.legend(loc='best', fontsize=6)
 
-    col1sub, col2sub = st.columns([2, 2])
-    with col1sub:
-      st.subheader('Training Set')
+#     col1sub, col2sub = st.columns([2, 2])
+#     with col1sub:
+#       st.subheader('Training Set')
         
-      fig, ax = plt.subplots(nrows=1, ncols=1)
-      _ = ax.plot(y_train, model.predict(X_train), 'o', ms=1)
-      _ = ax.plot(y_train, y_train, '-', color='k', lw=1)
-      ax.set_xlabel('Actual Value')
-      ax.set_ylabel('Predicted Value')
-      ax.set_title(r'Training Set ($R^2=$'+f"{'%.3f'%model.score(X_train, y_train)})")
-      configure_plot(ax)
+#       fig, ax = plt.subplots(nrows=1, ncols=1)
+#       _ = ax.plot(y_train, model.predict(X_train), 'o', ms=1)
+#       _ = ax.plot(y_train, y_train, '-', color='k', lw=1)
+#       ax.set_xlabel('Actual Value')
+#       ax.set_ylabel('Predicted Value')
+#       ax.set_title(r'Training Set ($R^2=$'+f"{'%.3f'%model.score(X_train, y_train)})")
+#       configure_plot(ax)
 
-      fig, ax = plt.subplots(nrows=1, ncols=1)
-      resid = model.predict(X_train) - y_train
-      _ = ax.hist(resid, bins=20, density=True)
-      ax.set_xlabel(r'$y_{predicted} - y_{actual}$')
-      ax.set_ylabel('Frequency')
-      fit_gaussian(resid, ax)
-      configure_plot(ax)
+#       fig, ax = plt.subplots(nrows=1, ncols=1)
+#       resid = model.predict(X_train) - y_train
+#       _ = ax.hist(resid, bins=20, density=True)
+#       ax.set_xlabel(r'$y_{predicted} - y_{actual}$')
+#       ax.set_ylabel('Frequency')
+#       fit_gaussian(resid, ax)
+#       configure_plot(ax)
 
-    with col2sub:
-      st.subheader('Test Set')
+#     with col2sub:
+#       st.subheader('Test Set')
 
-      fig, ax = plt.subplots(nrows=1, ncols=1)
-      _ = ax.plot(y_test, model.predict(X_test), 'o', ms=1)
-      _ = ax.plot(y_test, y_test, '-', color='k', lw=1)
-      ax.set_xlabel('Actual Value')
-      ax.set_ylabel('Predicted Value')
-      ax.set_title(r'Test Set ($R^2=$'+f"{'%.3f'%model.score(X_test, y_test)})")
-      configure_plot(ax)
+#       fig, ax = plt.subplots(nrows=1, ncols=1)
+#       _ = ax.plot(y_test, model.predict(X_test), 'o', ms=1)
+#       _ = ax.plot(y_test, y_test, '-', color='k', lw=1)
+#       ax.set_xlabel('Actual Value')
+#       ax.set_ylabel('Predicted Value')
+#       ax.set_title(r'Test Set ($R^2=$'+f"{'%.3f'%model.score(X_test, y_test)})")
+#       configure_plot(ax)
 
-      fig, ax = plt.subplots(nrows=1, ncols=1)
-      resid = model.predict(X_test) - y_test
-      _ = ax.hist(resid, bins=20, density=True)
-      ax.set_xlabel(r'$y_{predicted} - y_{actual}$')
-      ax.set_ylabel('Frequency')
-      fit_gaussian(resid, ax)
-      configure_plot(ax)
+#       fig, ax = plt.subplots(nrows=1, ncols=1)
+#       resid = model.predict(X_test) - y_test
+#       _ = ax.hist(resid, bins=20, density=True)
+#       ax.set_xlabel(r'$y_{predicted} - y_{actual}$')
+#       ax.set_ylabel('Frequency')
+#       fit_gaussian(resid, ax)
+#       configure_plot(ax)
 
-  with coef_tab:
-    fig, ax = plt.subplots()
-    ranked_features = sorted(zip(model.coef_, feature_names), key=lambda x:np.abs(x[0]), reverse=True)
-    _ = ax.bar(
-      x=np.arange(1, len(model.coef_)+1),
-      height=[x[0] for x in ranked_features],
-      align='center'
-    )
-    ax.set_xticks(np.arange(1, len(model.coef_)+1), [x[1] for x in ranked_features], rotation=90)
-    configure_plot(ax, size=(int(round(len(model.coef_)/4.)),2))
+#   with coef_tab:
+#     fig, ax = plt.subplots()
+#     ranked_features = sorted(zip(model.coef_, feature_names), key=lambda x:np.abs(x[0]), reverse=True)
+#     _ = ax.bar(
+#       x=np.arange(1, len(model.coef_)+1),
+#       height=[x[0] for x in ranked_features],
+#       align='center'
+#     )
+#     ax.set_xticks(np.arange(1, len(model.coef_)+1), [x[1] for x in ranked_features], rotation=90)
+#     configure_plot(ax, size=(int(round(len(model.coef_)/4.)),2))

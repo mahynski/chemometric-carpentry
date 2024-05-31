f755bdb1b1edc05116db41f513673156fb53a251
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-31 10:46:24 -0400

    bokeh test

--- streamlit/periodic-table.py
+++ streamlit/periodic-table.py
@@ -236,88 +236,12 @@ if uploaded_file is not None:
 
       mouseover = st.toggle('Mouseover Properties', value=False, key='mouseover')
 
-      return fig
+      return fig, cluster_id_to_feature_ids
 
-    fig = recompute()
+    fig, cluster_id_to_feature_ids = recompute()
 
   with col2:
     st.pyplot(fig, use_container_width=True)
-  # st.bokeh_chart(p)
-
-# periods = ["I", "II", "III", "IV", "V", "VI", "VII"]
-# groups = [str(x) for x in range(1, 19)]
-
-# df = elements.copy()
-# df["atomic mass"] = df["atomic mass"].astype(str)
-# df["group"] = df["group"].astype(str)
-# df["period"] = [periods[x-1] for x in df.period]
-# df = df[df.group != "-"]
-# df = df[df.symbol != "Lr"]
-# df = df[df.symbol != "Lu"]
-
-# cmap = {
-#     "alkali metal"         : "#a6cee3",
-#     "alkaline earth metal" : "#1f78b4",
-#     "metal"                : "#d93b43",
-#     "halogen"              : "#999d9a",
-#     "metalloid"            : "#e08d49",
-#     "noble gas"            : "#eaeaea",
-#     "nonmetal"             : "#f1d4Af",
-#     "transition metal"     : "#599d7A",
-# }
-
-# TOOLTIPS = [
-#     ("Name", "@name"),
-#     ("Atomic number", "@{atomic number}"),
-#     ("Atomic mass", "@{atomic mass}"),
-#     ("Type", "@metal"),
-#     ("CPK color", "$color[hex, swatch]:CPK"),
-#     ("Electronic configuration", "@{electronic configuration}"),
-# ]
-
-# p = figure(title="Periodic Table (omitting LA and AC Series)", width=1000, height=450,
-#            x_range=groups, y_range=list(reversed(periods)),
-#            tools="hover", toolbar_location=None, tooltips=TOOLTIPS)
-
-# r = p.rect("group", "period", 0.95, 0.95, source=df, fill_alpha=0.6, legend_field="metal",
-#            color=factor_cmap('metal', palette=list(cmap.values()), factors=list(cmap.keys())))
-
-# text_props = dict(source=df, text_align="left", text_baseline="middle")
-
-# x = dodge("group", -0.4, range=p.x_range)
-
-# p.text(x=x, y="period", text="symbol", text_font_style="bold", **text_props)
-
-# p.text(x=x, y=dodge("period", 0.3, range=p.y_range), text="atomic number",
-#        text_font_size="11px", **text_props)
-
-# p.text(x=x, y=dodge("period", -0.35, range=p.y_range), text="name",
-#        text_font_size="7px", **text_props)
-
-# p.text(x=x, y=dodge("period", -0.2, range=p.y_range), text="atomic mass",
-#        text_font_size="7px", **text_props)
-
-# p.text(x=["3", "3"], y=["VI", "VII"], text=["LA", "AC"], text_align="center", text_baseline="middle")
-
-# p.outline_line_color = None
-# p.grid.grid_line_color = None
-# p.axis.axis_line_color = None
-# p.axis.major_tick_line_color = None
-# p.axis.major_label_standoff = 0
-# p.legend.orientation = "horizontal"
-# p.legend.location ="top_center"
-# p.hover.renderers = [r] # only hover element boxes
-
-# def recompute(attr, old, new):
-#   return
-
-# t_slider = Slider(
-#                 start=0,
-#                 end=2,
-#                 value=0,  # Start visualization from t=0
-#                 step=0.1,
-#                 title="t value",
-#             )
-# t_slider.on_change("value", recompute)
-# st.bokeh_chart(t_slider)
-# st.bokeh_chart(p)
+    
+  st.divider()
+  

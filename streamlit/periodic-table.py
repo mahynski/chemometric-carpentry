82687dd169d2ade091c53ab0f81e33a75391322e
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-31 11:13:44 -0400

    bokeh test

--- streamlit/periodic-table.py
+++ streamlit/periodic-table.py
@@ -107,7 +107,7 @@ if uploaded_file is not None:
     p = figure(
       title="",
       width=1000,
-      height=295,
+      height=292,
       x_range=groups,
       y_range=list(reversed(periods)),
       tools="hover" if hover else "",

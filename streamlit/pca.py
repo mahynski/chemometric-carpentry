dde098fe9621face244aa0af001dff768364e6f7
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-31 21:43:21 -0400

    ellipse test

--- streamlit/pca.py
+++ streamlit/pca.py
@@ -277,8 +277,8 @@ if (test_size > 0):
             largest_eval = ordered[0][0]
             largest_evec = ordered[0][1]
             angle = np.arctan2(largest_evec[1], largest_evec[0])*180.0/np.pi
-            st.write(angle, class_center, d_crit, ordered[0][0], ordered[1][0])
-            ell = Ellipse(xy=class_center, width=np.sqrt(ordered[0][0]*d_crit)**2, height=np.sqrt(ordered[1][0]*d_crit), angle=angle)
+            st.write(S)
+            ell = Ellipse(xy=class_center, width=np.sqrt(ordered[0][0])*d_crit, height=np.sqrt(ordered[1][0])*d_crit, angle=angle)
             ax.add_artist(ell)
 
 

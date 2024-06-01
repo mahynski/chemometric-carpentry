78f8e88d45ddd4a462e83f2f690f672cb60be9bd
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-05-31 21:59:48 -0400

    ellipse test

--- streamlit/pca.py
+++ streamlit/pca.py
@@ -265,7 +265,7 @@ if (test_size > 0):
               class_center, S, d_crit,
               rmax=np.sqrt(d_crit * np.max(np.diag(S))) * 1.2,
               rbins=100,
-              tbins=180,
+              tbins=30,
             )
             ax.plot(cutoff[:, 0], cutoff[:, 1], color=f'C{i}', lw=1, alpha=0.0) # best to use boundary too because it helps with plotting
 

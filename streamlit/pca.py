33ec8e87bf1f2598f76a78d46b4907e209875b48
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-06-02 14:53:54 -0400

    ellipse test

--- streamlit/pca.py
+++ streamlit/pca.py
@@ -281,7 +281,7 @@ if (test_size > 0):
             ax.add_artist(ell)
             # ell.set_clip_box(ax.bbox)
             ell.set_alpha(0.3)
-            ell.set_facecolor(f'C{i}')
+            ell.set_facecolor(None)#f'C{i}')
             ell.set_edgecolor('k')
 
 

ffe39282b1b3d7a63311179a2e03d54e0799fb2c
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-06-05 13:08:35 -0400

    added pls descr

--- streamlit/pls.py
+++ streamlit/pls.py
@@ -48,9 +48,9 @@ with col1_:
     [Partial least squares (PLS) regression](https://en.wikipedia.org/wiki/Partial_least_squares_regression) is also known as "projection to latent structures". There are several variations on the algorithm which are available in [sklearn](https://scikit-learn.org/stable/modules/cross_decomposition.html), however the commonly used "PLS1" (if y has one column) or "PLS2" (if y has multiple columns) are a form of [PLSRegression](https://scikit-learn.org/stable/modules/cross_decomposition.html#plsregression).
 
     In essence PLS is actually a scheme to project both X and Y while taking each other into account.  Assume we model **centered matrices** $X$ and $Y$ as follows:
-
+    
         $$X = TP^T + E,$$
-
+        
         $$Y = TQ^T + F,$$
 
     where $E$ and $F$ are error terms (assumed to be IID). $X$ has dimensions $n \times p$, and $Y$ has dimensions $n \times l$; $T$ is the $n \times k$ projection matrix of $X$, which is computed by taking both $X$ and $Y$ into account.  Here, $k \le p$ represents a dimensionality reduction; while $P$ is $p \times k$ and $Q$ is $l \times k$. 

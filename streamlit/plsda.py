9d41a2b4057f9338e191e7a5f414ed8acd10468b
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-06-07 13:25:09 -0400

    plsda test

--- streamlit/plsda.py
+++ streamlit/plsda.py
@@ -193,7 +193,7 @@ if (test_size > 0) and (style is not None) and (target_column is not None):
             for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels()):
                 item.set_fontsize(6)
             fig = plt.gcf()
-            fig.set_size_inches(3, 3)
+            fig.set_size_inches(2, 2)
             plt.legend(fontsize=6, bbox_to_anchor=(1,1))
             st.pyplot(fig, use_container_width=False)
 
